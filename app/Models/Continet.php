a420db62ea1defc14c8a7c882f49d2f8c1747ee0
Your Name <you@example.com>
2022-08-04 11:55:53 -0500

    Creacion de modelos

---
+++ app/Models/Continet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Continet extends Model
+{
+    protected $table = "continents";
+    use HasFactory;
+}
